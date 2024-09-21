95b45cee7359c1c8bbacb778840a1553aa540258
Farnahad <123350114+Farnahad@users.noreply.github.com>
2024-09-21 15:21:57 +0330

    vsd

---
+++ SecurityStudio.Service.Base/Log/ILogService.cs
@@ -0,0 +1,12 @@
+﻿using SecurityStudio.Base.Main.Service;
+
+namespace SecurityStudio.Service.Base.Log
+{
+    public interface ILogService : IService
+    {
+        void Info(string info);
+        void Warning(string warning);
+        void Error(string error);
+        void Error(Exception exception);
+    }
+}
